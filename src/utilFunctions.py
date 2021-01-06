4d6a729b99c416d3d456e968dde298482af25974
Sanjay-R <sanjayrachwani@yahoo.com>
2021-01-06 15:39:35 +0100

    pandas corr function

--- src/utilFunctions.py
+++ src/utilFunctions.py
@@ -7,4 +7,14 @@ def pearson(userMovie):
 
     pe = np.ma.corrcoef(np.ma.masked_invalid(userMovie))
     print(pe[:40, :50])
-    return pe
+
+    print("user 1: " , np.std(userMovie[1]))
+    print("user 6038: " , np.std(userMovie[6038]))
+
+    temp = sim(5, 8)
+    
+    return temp
+
+def sim(x, y):
+
+    return x+y
