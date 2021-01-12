2cc6361cdb1981da2fb29f3c174af939520f390a
Borna <B.Salarian@student.tudelft.nl>
2021-01-12 19:09:37 +0100

    WIP: score for a movie for a user

--- src/step1-recommender-systems.py
+++ src/step1-recommender-systems.py
@@ -17,19 +17,24 @@ To know more about the expectations, please refer to the guidelines.
 ##
 #####
 
-#Where data is located
+# Where data is located
 movies_file = 'movies.csv'
 users_file = 'users.csv'
 ratings_file = 'ratings.csv'
 predictions_file = 'predictions.csv'
 submission_file = 'submission.csv'
 
-
 # Read the data using pandas
-movies_description = pd.read_csv(movies_file, delimiter=';', dtype={'movieID':'int', 'year':'int', 'movie':'str'}, names=['movieID', 'year', 'movie'])
-users_description = pd.read_csv(users_file, delimiter=';', dtype={'userID':'int', 'gender':'str', 'age':'int', 'profession':'int'}, names=['userID', 'gender', 'age', 'profession'])
-ratings_description = pd.read_csv(ratings_file, delimiter=';', dtype={'userID':'int', 'movieID':'int', 'rating':'int'}, names=['userID', 'movieID', 'rating'])
+movies_description = pd.read_csv(movies_file, delimiter=';', dtype={'movieID': 'int', 'year': 'int', 'movie': 'str'},
+                                 names=['movieID', 'year', 'movie'])
+users_description = pd.read_csv(users_file, delimiter=';',
+                                dtype={'userID': 'int', 'gender': 'str', 'age': 'int', 'profession': 'int'},
+                                names=['userID', 'gender', 'age', 'profession'])
+ratings_description = pd.read_csv(ratings_file, delimiter=';',
+                                  dtype={'userID': 'int', 'movieID': 'int', 'rating': 'int'},
+                                  names=['userID', 'movieID', 'rating'])
 predictions_description = pd.read_csv(predictions_file, delimiter=';', names=['userID', 'movieID'], header=None)
+
 
 #####
 ##
@@ -39,53 +44,56 @@ predictions_description = pd.read_csv(predictions_file, delimiter=';', names=['u
 
 def predict_collaborative_filtering(movies, users, ratings, predictions):
     # TO COMPLETE
-    
-    #userRatingMatrix => merge users and their ratings
+
+    # userRatingMatrix => merge users and their ratings
     uRM = pd.merge(users, ratings, on='userID')
     # print(uRM)
     # print("\n\n\n")
 
-
-    #userRatingMoviesMatrix => merge users+ratings on the movies they watched
+    # userRatingMoviesMatrix => merge users+ratings on the movies they watched
     uRMM = pd.merge(uRM, movies, on='movieID')
     # print(uRMM)
     # print("\n\n\n")
 
-    userMovie = uRMM.pivot(index = 'movieID', columns= 'userID', values= 'rating')
+    userMovie = uRMM.pivot(index='movieID', columns='userID', values='rating')
 
-    #TODO: REMOVE THIS TEMP 500X500 CUT
-    um = userMovie.iloc[:500,:500]
+    # TODO: REMOVE THIS TEMP 500X500 CUT
+    um = userMovie.iloc[:500, :500]
 
-    #user-user collaborative matrix
+    # user-user collaborative matrix
     utilMatrix = uf.pearson(um)
 
     # print(utilMatrix)
 
     thres = uf.threshold(0.2, 50, utilMatrix)
 
-    top = uf.selectTopNeighbors(50,utilMatrix)
+    top = uf.selectTopNeighbors(50, utilMatrix)
 
+    predict_test = uf.score(um, utilMatrix, um.loc['1914'], top.loc['1914'], 1635)
 
     # print(utilMatrix)
     print(uf.normalized_data(userMovie))
     print(thres)
+    print(predict_test)
 
     return top
 
+
 predict_collaborative_filtering(movies_description, users_description, ratings_description, predictions_description)
+
 
 #####
 ##
 ## LATENT FACTORS
 ##
 #####
-    
+
 def predict_latent_factors(movies, users, ratings, predictions):
     ## TO COMPLETE
 
     pass
-    
-    
+
+
 #####
 ##
 ## FINAL PREDICTORS
@@ -93,9 +101,9 @@ def predict_latent_factors(movies, users, ratings, predictions):
 #####
 
 def predict_final(movies, users, ratings, predictions):
-  ## TO COMPLETE
+    ## TO COMPLETE
 
-  pass
+    pass
 
 
 #####
@@ -104,12 +112,13 @@ def predict_final(movies, users, ratings, predictions):
 ## //!!\\ TO CHANGE
 ##
 #####
-    
-#By default, predicted rate is a random classifier
+
+# By default, predicted rate is a random classifier
 def predict_random(movies, users, ratings, predictions):
     number_predictions = len(predictions)
 
     return [[idx, randint(1, 5)] for idx in range(1, number_predictions + 1)]
+
 
 #####
 ##
@@ -120,12 +129,12 @@ def predict_random(movies, users, ratings, predictions):
 ## //!!\\ TO CHANGE by your prediction function
 predictions = predict_random(movies_description, users_description, ratings_description, predictions_description)
 
-#Save predictions, should be in the form 'list of tuples' or 'list of lists'
+# Save predictions, should be in the form 'list of tuples' or 'list of lists'
 with open(submission_file, 'w') as submission_writer:
-    #Formates data
+    # Formates data
     predictions = [map(str, row) for row in predictions]
     predictions = [','.join(row) for row in predictions]
-    predictions = 'Id,Rating\n'+'\n'.join(predictions)
-    
-    #Writes it dowmn
+    predictions = 'Id,Rating\n' + '\n'.join(predictions)
+
+    # Writes it dowmn
     submission_writer.write(predictions)
--- src/utilFunctions.py
+++ src/utilFunctions.py
@@ -3,57 +3,60 @@ import pandas as pd
 import os.path
 from random import randint
 
-def pearson(userMovie):
 
+def pearson(userMovie):
     return userMovie.corr(method="pearson")
 
-def threshold(t : float, neighbors :int, df : pd.DataFrame):
 
-    #lower limit for neighbors is 50
+def threshold(t: float, neighbors: int, df: pd.DataFrame):
+    # lower limit for neighbors is 50
     neighbors = max(50, neighbors)
 
     ret = df.apply(lambda row: seriesLargest(neighbors, t, row), axis=1)
 
     return ret
 
-def selectTopNeighbors(neighbors, df):
 
+def selectTopNeighbors(neighbors, df):
     ret = df.apply(lambda row: seriesLargestTop(neighbors, row), axis=1)
 
     return ret
 
-def seriesLargestTop(neighbors: int, row : pd.Series):
+
+def seriesLargestTop(neighbors: int, row: pd.Series):
     s = row.nlargest(neighbors + 1, keep='all').head(neighbors + 1).index.to_numpy()
     s = np.delete(s, np.argwhere(s == row.name))
     # If there aren't enough neighbors, pad the array with zeros up to neighbors
     s = np.pad(s, (0, max(0, (neighbors - s.size))), 'constant', constant_values=(0, 0))
     return s
 
-def seriesLargest(neighbors : int, t : float, row : pd.Series):
 
-    #Filter out all elements that are below or equal to the threshold
-    s = row[(row > t)].nlargest(neighbors+1, keep='all').head(neighbors+1).index.to_numpy()
+def seriesLargest(neighbors: int, t: float, row: pd.Series):
+    # Filter out all elements that are below or equal to the threshold
+    s = row[(row > t)].nlargest(neighbors + 1, keep='all').head(neighbors + 1).index.to_numpy()
 
     # print("\n\n____\n", s, " size=", s.size)
 
-    #Ignore your own value (user 3 - user 3 bijv), which is also why we take neighbors+1 largest values
-    #Use row.name to get the index
-    s = np.delete(s, np.argwhere(s==row.name))
+    # Ignore your own value (user 3 - user 3 bijv), which is also why we take neighbors+1 largest values
+    # Use row.name to get the index
+    s = np.delete(s, np.argwhere(s == row.name))
 
     # print("\n", s , " size=", s.size)
 
-    #If there aren't enough neighbors, pad the array with zeros up to neighbors
-    s = np.pad(s, (0, max(0, (neighbors - s.size))), 'constant', constant_values = (0,0))
+    # If there aren't enough neighbors, pad the array with zeros up to neighbors
+    s = np.pad(s, (0, max(0, (neighbors - s.size))), 'constant', constant_values=(0, 0))
 
     # print ("=> \n", s , " size=", s.size)
 
     return s
 
+
 def normalized_data(df: pd.DataFrame):
     df_mean = df.mean(axis=1)
-    df_normal = df.subtract(df_mean, axis = 'rows')
+    df_normal = df.subtract(df_mean, axis='rows')
     # print(df_normal)
     return df_normal
+
 
 def normalized_row(user: pd.Series):
     user_mean = user.mean
@@ -61,31 +64,40 @@ def normalized_row(user: pd.Series):
     # return user_normal
     pass
 
-def scoreRow(userMovies: pd.DataFrame, ratings: pd.Series , neighbors: pd.Series , movie: pd.Index):
-    ratings_avg = ratings.mean
+
+def score(userMovies: pd.DataFrame,
+          correlation: pd.DataFrame, ratings: pd.Series, neighbors: pd.Series, movie):
     # normalized_data(userMovies)
     # User 42 : [Nan ,Nan , 0.45 , Nan , 0.85]
 
-    for movie_rating in ratings:
-        if movie_rating.isnull:
-            movie_rating_predic = userMovies.iloc[neighborsID , movie]
+    if movie in ratings:
+        return ratings[movie]
 
-    score = np.dot()
-    pass
+    ratings_avg = ratings.mean
+    sim_sum = 0
+    for n in neighbors:
+        sim_sum += correlation.at[movie, n]
+
+    sim_times_rating = 0
+    for n in neighbors:
+        sim_times_rating += (correlation.at[movie, n]
+                             * (userMovies.at[movie, n] - userMovies[n].mean))
+
+    score = ratings_avg + (sim_times_rating / sim_sum)
+
+    return score
 
 
-def scoreItem(df: pd.DataFrame ,user: pd.Series
-              ,neighbor_rating, neighbor_similarity, ratings):
-
-    #We will be using the weighted average method to compute the score.
-    #We want to only score items that have not been scored yet.
+def scoreItem(df: pd.DataFrame, user: pd.Series
+              , neighbor_rating, neighbor_similarity, ratings):
+    # We will be using the weighted average method to compute the score.
+    # We want to only score items that have not been scored yet.
     # user_norm = normalized_row(user)
     # avg_ratings = user.mean
     #
     # selectTopNeighbors(df)
 
-
-    #Two dataframes: Neighbors and Ratings
+    # Two dataframes: Neighbors and Ratings
 
     df_normal = normalized_data(df)
 
